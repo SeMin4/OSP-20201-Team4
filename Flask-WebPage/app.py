12130d2f41ac6ea9a7ca629d81d901d1b28c2e9b
SeMin <ohsemin4@gmail.com>
2020-06-05 17:14:34 +0900

    Flask 기반 초기 웹페이지 구성

--- Flask-WebPage/app.py
+++ Flask-WebPage/app.py
@@ -2,10 +2,21 @@ from flask import Flask
 from flask import render_template
 from flask import redirect, url_for, abort
 from flask import request
+from flask_dropzone import Dropzone
+
 
 
 app = Flask(__name__)
+dropzone = Dropzone(app=app)
+
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'text'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload/file', methods=['GET', 'POST'])
+def getFibonacci():
+    error = None
+    if request.method == 'POST':
+        return
