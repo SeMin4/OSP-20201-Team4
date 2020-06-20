787606f5072b06acb1c267f609e23b066dd17807
SeMin <ohsemin4@gmail.com>
2020-06-20 15:54:05 +0900

    project 마무리

--- shell.sh
+++ shell.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
 
-#cp -r Flask-WebPage ~/
-#cp -r elasticsearch.yml ~/
-cp -r etcRequirement.py ~/
+mv Flask-WebPage ~/
+mv elasticsearch.yml ~/
+mv etcRequirement.py ~/
 cd ~
-#mkdir osp-team4
-#cp -r Flask-WebPage elasticsearch.yml osp-team4/
-cp -r etcRequirement.py osp-team4/
+mkdir osp-team4
+mv Flask-WebPage elasticsearch.yml osp-team4/
+mv etcRequirement.py osp-team4/
 cd osp-team4
 
-#wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.2-linux-x86_64.tar.gz
-#tar xvzf elasticsearch-7.6.2-linux-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.2-linux-x86_64.tar.gz
+tar xvzf elasticsearch-7.6.2-linux-x86_64.tar.gz
 
-#cp elasticsearch.yml elasticsearch-7.6.2/config/ # we have to modify cp command
+mv elasticsearch.yml elasticsearch-7.6.2/config/ # we have to modify cp command
 
 ./elasticsearch-7.6.2/bin/elasticsearch -d
 
-python etcRequirement.py
-
-cd Flask-WebPage
 
 
 echo "flask directory archiecture done"
 
 
 echo "Installing flask..."
-#pip install flask
+pip install flask
 
 
 echo "Installing Werkzeug..."
-#pip install -U Werkzeug
+pip install -U Werkzeug
 
 echo "Installing beautifulsoup4..."
-#pip install beautifulsoup4
+pip install beautifulsoup4
 
 echo "Installing nltk..."
-#pip install nltk
+pip install nltk
 
 echo "Installing wordcloud"
-#pip install wordcloud
+pip install wordcloud
 
 echo "Installing matplotlib"
-#pip install matplotlib
+pip install matplotlib
 
 echo "Installing elasticsearch"
-#pip install elasticsearch
+pip install elasticsearch
 
 echo "Installing requests"
-#pip install requests
+pip install requests
 
 echo "Installing pandas"
-#pip install pandas
+pip install pandas
 
+python etcRequirement.py
 
-flask run
+cd Flask-WebPage
+
+python -m flask run
